865a7eb6def1ce965b2a8d446b3cad21ed2f0186
max <max.assis@chatguru.com.br>
2025-07-09 19:40:20 -0300

    feat: corrigido bug na navegação

--- app/(app)/(configurations)/_layout.tsx
+++ app/(app)/(configurations)/_layout.tsx
@@ -1,10 +1,5 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="configInit" />
-      <Stack.Screen name="configEdit" />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
